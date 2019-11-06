d954fc49ebba55fef3a7e61632a430cc7269422d
Lidii <lidiiany92@gmail.com>
2019-11-05 17:22:03 -0800

    fetching users

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,9 +1,21 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import NavBar from './components/NavBar/NavBar'
 import Users from './components/Users/Users';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    this.setState({loading: true});
+    const res = await axios.get('https://github.com/defunkt/users');
+    this.setState({users: res.data, loading: false});
+  }
 
   render() {
       return (
