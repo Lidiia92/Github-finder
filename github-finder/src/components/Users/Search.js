f0f7149b3594a211398d93ba917ac6e81b4a8ebd
Lidii <lidiiany92@gmail.com>
2019-11-07 10:23:14 -0800

    clearing users from state

--- github-finder/src/components/Users/Search.js
+++ github-finder/src/components/Users/Search.js
@@ -1,9 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
 
     state = {
         text: '',
+    }
+
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired,
     }
 
     onChange = (e) => {
@@ -22,6 +29,7 @@ export class Search extends Component {
                     <input type="text" name="text" placeholder="Search Users" value={this.state.text} onChange={this.onChange}/>
                     <input type="submit" value="Search" className="btn btn-dark btn-block" />
                 </form>
+                {this.props.showClear && <button className="btn btn-light btn-block" onClick={this.props.clearUsers}>Clear</button>}
             </div>
         )
     }
