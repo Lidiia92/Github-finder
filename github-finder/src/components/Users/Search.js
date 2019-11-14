ffab4bfcdf718f031a293b601658d6f829b987c6
Lidii <lidiiany92@gmail.com>
2019-11-13 16:56:10 -0800

    refactored Serch component to hooks

--- github-finder/src/components/Users/Search.js
+++ github-finder/src/components/Users/Search.js
@@ -1,38 +1,35 @@
-import React  from 'react';
+import React, {useState}  from 'react';
 import PropTypes from 'prop-types';
 
-const Search = () => {
+const Search = ({searchUsers, clearUsers, showClear, setAlert}) => {
 
-    state = {
-        text: '',
+    const [text, setText] = useState('');
+
+    const onChange = (e) => {
+        setText(e.target.value)
     }
 
-    onChange = (e) => {
-        this.setState({ [e.target.name]: e.target.value })
-    }
-
-    onSubmit = (e) => {
+    const onSubmit = (e) => {
         e.preventDefault()
 
-        if (this.state.text === '') {
-            this.props.setAlert('Please enter GitHub username', 'light')
+        if (text === '') {
+            setAlert('Please enter GitHub username', 'light')
         } else {
-            this.props.searchUsers(this.state.text);
-            this.setState({text: ''});
+            searchUsers(text);
+            setText('');
         }
 
     }
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.onSubmit} className="form">
-                    <input type="text" name="text" placeholder="Search Users" value={this.state.text} onChange={this.onChange}/>
-                    <input type="submit" value="Search" className="btn btn-dark btn-block" />
-                </form>
-                {this.props.showClear && <button className="btn btn-light btn-block" onClick={this.props.clearUsers}>Clear</button>}
-            </div>
-        )
-    }
+   
+    return (
+        <div>
+            <form onSubmit={onSubmit} className="form">
+                <input type="text" name="text" placeholder="Search Users" value={text} onChange={onChange}/>
+                <input type="submit" value="Search" className="btn btn-dark btn-block" />
+            </form>
+            {showClear && <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>}
+        </div>
+    )
 }
 
 Search.propTypes = {
